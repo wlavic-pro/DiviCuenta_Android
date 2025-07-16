10c29da60dbd1393ca7a5b2e4a56ede9eb0b29b3
wlavic-pro <wlady8461@gmail.com>
2025-07-16 14:22:46 -0400

    Replace corrupted Android files and update README

--- app/src/main/java/com/example/divicuenta_v2/MainActivity.kt
+++ app/src/main/java/com/example/divicuenta_v2/MainActivity.kt
@@ -1,1 +1,11 @@
-¥§$jœ¢Å©©•Šøœ¹éíkûöŠjh®Ö§vº"tž'Þ²Ø¦¦Šíjwk¢'r‰íz{
+package com.example.divicuenta_v2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
